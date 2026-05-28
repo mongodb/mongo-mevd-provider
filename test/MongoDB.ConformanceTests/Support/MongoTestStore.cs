a39a7c2ea2f47f623b44c3bbcfa03d7d396546c5
Arthur Vickers <ajcvickers@hotmail.com>
2026-05-28 12:46:47 +0100

    [review-iter 2] Address review findings

--- test/MongoDB.ConformanceTests/Support/MongoTestStore.cs
+++ test/MongoDB.ConformanceTests/Support/MongoTestStore.cs
@@ -5,6 +5,7 @@ using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Microsoft.Extensions.VectorData;
 using MongoDB.VectorData;
 using MongoDB.Bson;
@@ -13,6 +14,16 @@ using Testcontainers.MongoDb;
 using VectorData.ConformanceTests.Support;
 
 namespace MongoDB.VectorData.ConformanceTests.Support;
+
+// Force MongoTestStore's static constructor to run as soon as the conformance test assembly loads. The cctor
+// installs MongoCollectionTestHook resolvers; without this initializer those resolvers would be null until the
+// first member of MongoTestStore is accessed, leaving any MongoCollection constructed earlier (e.g. via DI
+// registrations resolving MongoCollection<TKey, TRecord> directly) silently bypassing the test hook.
+internal static class MongoTestStoreInitializer
+{
+    [ModuleInitializer]
+    internal static void Init() => RuntimeHelpers.RunClassConstructor(typeof(MongoTestStore).TypeHandle);
+}
 
 #pragma warning disable CA1001 // Type owns disposable fields but is not disposable
 
